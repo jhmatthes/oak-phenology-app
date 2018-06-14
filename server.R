89ef8c379be0f738570c3952ff6acaf23df5be54
jaclyn.matthes <jaclyn.hatala.matthes@gmail.com>
2018-06-14 15:38:06 -0400

    First commit for app to github

---
+++ server.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+shinyServer(
+  function(input, output){
+    
+    output$myplot <- renderPlot({
+      
+      # Calculate total individuals in that phenophase (# of yes)
+      # Total in status by day
+      sampSize <- oak_phe %>% 
+        filter(phenophaseName == input$phenphase) %>%
+        count(date, siteID)
+      
+      inStat <- oak_phe %>% 
+        filter(phenophaseName == input$phenphase) %>%
+        group_by(date, siteID) %>%
+        count(phenophaseStatus)
+      
+      inStat <- full_join(sampSize, inStat, by=c("date", "siteID"))
+      
+      # Retain only Yes
+      inStat_T <- filter(inStat, phenophaseStatus %in% "yes")
+      
+      # Plot percent of leaves in phenophase ---------------------------------------
+      # convert to percent
+      inStat_T$percent<- ((inStat_T$n.y)/inStat_T$n.x)*100
+      
+      phenoPlot_P <- ggplot(filter(inStat_T, date > "2016-01-01")) +
+        geom_jitter(aes(x = date, y = percent, color = siteID)) + 
+        ggtitle(paste("Proportion with",input$phenphase)) +
+        xlab("Date") + ylab("% of Individuals") +
+        theme(plot.title = element_text(lineheight=.8, face="bold", size = 20)) +
+        theme(text = element_text(size=14)) + 
+        scale_color_manual(name = "Site",
+                           values = c("forestgreen","darkorchid4","darkgoldenrod"),
+                           labels = c("Harvard Forest, MA", 
+                                      "Ordway-Swisher, FL",
+                                      "San Joaquin, CA"))
+      
+      phenoPlot_P
+      
+    })
+  }
+)
+    
+    
---
+++ ui.R
@@ -0,0 +1,47 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  titlePanel(div(HTML("Outstanding Oaks: <em>Quercus</em> phenology at NEON sites"))),
+  
+  sidebarLayout(
+    
+    sidebarPanel(    
+      selectInput("phenphase",label="Phenophase of interest",
+                  choices= phenophases),
+      width = 4),
+    
+  #Output functions
+  mainPanel(
+     p("This is a visualization of oak phenology from three sites in the ", 
+     a("National Ecological Observatory Network.", href="https://www.neonscience.org/", target="_blank")),
+     p(),
+     p("The data include phenological observations on 30 individuals each of red oak",
+     tags$em("(Quercus rubra)"),
+       "at the ",
+     a("Harvard Forest", href="https://www.neonscience.org/field-sites/field-sites-map/HARV", target="_blank"),
+     "in Massachusetts, turkey oak", 
+     tags$em("(Quercus laevis)"),
+     "at the ",
+     a("Ordway-Swisher Biological Station", href="https://www.neonscience.org/field-sites/field-sites-map/OSBS", target="_blank"),
+     "in Florida, and blue oak",
+     tags$em("(Quercus douglasii)"),
+     "at the ",
+     a("San Joaquin Experimental Range", href="https://www.neonscience.org/field-sites/field-sites-map/SJER", target="_blank"),
+     "in California."),
+     plotOutput("myplot"),
+     
+     tags$hr(),
+     tags$footer("Shiny app developed by: "),
+     tags$footer(p(a("Jackie Hatala Matthes", href="http://matthesecolab.com", target="_blank"),
+                 ", Assistant Professor of Biological Sciences, Wellesley College (2018) "),
+                 p("App source code available on ",a("GitHub",href = "https://github.com/jhmatthes/mammal-traits")),
+                 p(" "),
+                 tags$img(alt="Creative Commons License", style="border-width:0", src="https://i.creativecommons.org/l/by/4.0/88x31.png"),
+                 tags$body("This work is licensed under a", a("Creative Commons Attribution 4.0 International License", href="http://creativecommons.org/licenses/by/4.0/"),".")))
+   )
+
+  
+ 
+  )
+)
